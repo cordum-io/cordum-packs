6e5ef81ac6c903f6e447f29b2de41c90c552ae11
YARONT <YARONT111"GMAIL.COM>
2026-01-15 15:08:17 +0200

    feat(msteams-pack): add worker and tests

---
+++ packs/msteams/cmd/cordum-msteams/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cordum-io/cordum-packs/packs/msteams/internal/config"
+	"github.com/cordum-io/cordum-packs/packs/msteams/internal/worker"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("config error: %v", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	w, err := worker.New(cfg)
+	if err != nil {
+		log.Fatalf("worker init failed: %v", err)
+	}
+	defer w.Close()
+
+	if err := w.Run(ctx); err != nil && !errors.Is(err, context.Canceled) {
+		log.Fatalf("worker stopped: %v", err)
+	}
+}
